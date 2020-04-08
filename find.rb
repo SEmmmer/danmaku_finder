16704efd36bd2ee8b2e1182038ac5c10450fd982
Shirakami Emmmer <yjhdandan@163.com>
2020-04-08 18:40:47 +0800

    success

--- find.rb
+++ find.rb
@@ -7,10 +7,6 @@ class Danmaku
     @uid = uid
     @danmaku_context = danmaku
   end
-
-  def visit(&block)
-    block.call(self)
-  end
 end
 
 man = []
@@ -18,8 +14,8 @@ man[0] = 101957323
 man[1] = 9572567
 man[2] = 12892870
 
-ass_file = File.new("test.ass", "rb")
-
+ass_file = File.new("test.ass", "ab")
+start_time = 1586264660000
 
 file = File.new("2020-4-7.txt", "rb")
 if file
@@ -32,9 +28,21 @@ if file
     end
 
     i_array = i.split(":", 3)
-    i_danmaku = Danmaku.new(Integer(i_array[0]), Integer(i_array[1]), i_array[2])
+    i_danmaku = Danmaku.new(Integer(i_array[0]), Integer(i_array[1]), i_array[2].force_encoding("UTF-8"))
 
     if i_danmaku.uid == man[0] || i_danmaku.uid == man[1] || i_danmaku == man[2]
+      delta_time = i_danmaku.time - start_time
+      delta_time /= 10
+      ms = delta_time % 100
+      delta_time /= 100
+      s = delta_time % 60
+      delta_time /= 60
+      min = delta_time % 60
+      delta_time /= 60
+      h = delta_time
+
+      add_line = "Dialogue: 0,#{h}:#{min}:#{s}:#{ms},#{h}:#{min}:#{s + 1}:#{ms},吹雪 蓝白,,0,0,0,,#{i_danmaku.danmaku_context}"
+      ass_file.syswrite(add_line)
     end
   end
 else
