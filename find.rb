20e99a15062d43c5d3f487c48ea3fa5dbbb4c6db
Shirakami Emmmer <yjhdandan@163.com>
2020-04-08 13:47:58 +0800

    add danmaku class

--- find.rb
+++ find.rb
@@ -1,2 +1,19 @@
+class Danmaku
+  attr_accessor :time, :uid, :danmaku_context
 
-puts "hello"
+  def initialize(time, uid, danmaku = "")
+    @time = time
+    @uid = uid
+    @danmaku_context = danmaku
+  end
+
+  def visit(&block)
+    block.call(self)
+  end
+end
+
+ruby_tree = Danmaku.new(1919, 99999, "app")
+
+puts "Visiting a danmaku"
+ruby_tree.visit { |danmaku| puts danmaku.danmaku_context }
+puts
