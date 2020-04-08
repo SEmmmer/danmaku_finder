5bf883f895b9299ab163c0881856a6137673209d
Shirakami Emmmer <yjhdandan@163.com>
2020-04-08 17:38:12 +0800

    add function

--- find.rb
+++ find.rb
@@ -1,3 +1,4 @@
+# -*- coding:UTF-8 -*-
 class Danmaku
   attr_accessor :time, :uid, :danmaku_context
 
@@ -12,8 +13,23 @@ class Danmaku
   end
 end
 
-ruby_tree = Danmaku.new(1919, 99999, "app")
 
-puts "Visiting a danmaku"
-ruby_tree.visit { |danmaku| puts danmaku.danmaku_context }
-puts
+file = File.new("2020-4-7.txt", "rb")
+if file
+  file.each_line do
+  |i|
+    if i[0] == "T" || i[0] == "\n"
+      next
+    elsif i[0] == "S"
+      break
+    end
+    i_array = i.split(":", 3)
+    i_danmaku = Danmaku.new(Integer(i_array[0]), Integer(i_array[1]), i_array[2])
+
+
+  end
+else
+  puts "Failed to read file."
+end
+
+file.close
