b8768427775448b1baf58b765441989e139ad928
Shirakami Emmmer <yjhdandan@163.com>
2020-04-08 19:16:41 +0800

    fix

--- find.rb
+++ find.rb
@@ -13,9 +13,12 @@ man = []
 man[0] = 101957323
 man[1] = 9572567
 man[2] = 12892870
+# 向同传man致以崇高的敬意
 
 ass_file = File.new("test.ass", "ab")
 start_time = 1586264660000
+# 经计算，这个时间是2020-4-7 20：04
+
 
 file = File.new("2020-4-7.txt", "rb")
 if file
@@ -31,6 +34,9 @@ if file
     i_danmaku = Danmaku.new(Integer(i_array[0]), Integer(i_array[1]), i_array[2].force_encoding("UTF-8"))
 
     if i_danmaku.uid == man[0] || i_danmaku.uid == man[1] || i_danmaku == man[2]
+      danmaku_array = i_danmaku.danmaku_context.split("【", 2)
+      danmaku_array[1].delete!'】'
+
       delta_time = i_danmaku.time - start_time
       delta_time /= 10
       ms = delta_time % 100
@@ -41,8 +47,9 @@ if file
       delta_time /= 60
       h = delta_time
 
-      add_line = "Dialogue: 0,#{h}:#{min}:#{s}.#{ms},#{h}:#{min}:#{s + 1}.#{ms},吹雪 蓝白,,0,0,0,,#{i_danmaku.danmaku_context}"
+      add_line = "Dialogue: 0,#{h}:#{min}:#{s}.#{ms},#{h}:#{min}:#{s + 1}.#{ms},#{danmaku_array[0]},,0,0,0,,#{danmaku_array[1]}"
       ass_file.syswrite(add_line)
+      # puts add_line
     end
   end
 else
