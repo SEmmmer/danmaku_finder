58d20400a4ffdd4d1d58e27dab4700da01bc50fb
Shirakami Emmmer <yjhdandan@163.com>
2020-04-09 10:04:22 +0800

    find start time

--- find.rb
+++ find.rb
@@ -9,34 +9,21 @@ class Danmaku
   end
 end
 
-#day_before: 距离4月7日的天数
-# start_h: 开始的小时
-# start_h: 开始的分钟
-day_before = 1
-start_h = 21
-start_m = 59
-
-def time_calculate(day_before, start_h, start_m)
-  plus = day_before * 24 * 3600 * 1000
-  adjust = (start_h - 20) * 3600 * 1000 + (start_m - 4) * 60 * 1000
-  plus + adjust
-end
-
 man = []
 man[0] = 101957323
 man[1] = 9572567
 man[2] = 12892870
-man[3] = 33718180
+man[3] = 37718180
+man[4] = 2025122
 # 向同传man致以崇高的敬意
 
 ass_file = File.new("test.ass", "ab")
 
-# 经计算，这个时间是2020-4-7 20：04
 start_time = 1586264660000
-# 修正后的时间
-start_time += time_calculate(day_before, start_h, start_m)
-
-file = File.new("2020-4-7.txt", "rb")
+start_time = 1586354362515
+# 去文件里找start time
+#
+file = File.new("2020-4-9.txt", "rb")
 
 if file
   file.each_line do
@@ -50,9 +37,13 @@ if file
     i_array = i.split(":", 3)
     i_danmaku = Danmaku.new(Integer(i_array[0]), Integer(i_array[1]), i_array[2].force_encoding("UTF-8"))
 
-    if i_danmaku.uid == man[0] || i_danmaku.uid == man[1] || i_danmaku == man[2]
+    if i_danmaku.uid == man[4] || i_danmaku.uid == man[3]
       danmaku_array = i_danmaku.danmaku_context.split("【", 2)
-      danmaku_array[1].delete! '】'
+      if danmaku_array[1]
+        danmaku_array[1].delete! '】'
+      else
+        next
+      end
 
       delta_time = i_danmaku.time - start_time
       delta_time /= 10
@@ -66,13 +57,14 @@ if file
 
       format = danmaku_array[0]
       if danmaku_array[0].length == 0
-        format = ""
+        format = "吹雪"
         # 自定义Format
       end
 
       add_line = "Dialogue: 0,#{h}:#{min}:#{s}.#{ms},#{h}:#{min}:#{s + 1}.#{ms},#{format},,0,0,0,,#{danmaku_array[1]}"
-      # ass_file.syswrite(add_line)
+      ass_file.syswrite(add_line)
       # puts add_line
+      # puts i_danmaku.uid
       # 一技能实战
       # 二技能调试
     end
