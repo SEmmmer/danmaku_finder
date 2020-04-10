ab9d7e841805ceaf3d1fde2aba79e2ecce6ea0e6
Shirakami Emmmer <yjhdandan@163.com>
2020-04-10 11:38:10 +0800

    add man time fix

--- find.rb
+++ find.rb
@@ -15,13 +15,15 @@ man[1] = 9572567
 man[2] = 12892870
 man[3] = 37718180
 man[4] = 2025122
+man[5] = 171909416
+man[6] = 23420020
 # 向同传man致以崇高的敬意
 
 ass_file = File.new("test.ass", "ab")
 csv_file = File.new("test.csv", "wb")
 file = File.new("2020-4-9.txt", "rb")
 
-start_time = 1586354362515
+start_time = 1586444433208
 # 去文件里找start time
 
 if file
@@ -34,13 +36,9 @@ if file
     end
 
     i_array = i.split(":", 3)
-    # one = Integer(i_array[0])
-    # two = Integer(i_array[1])
-    # three = i_array[2].force_encoding("UTF-8")
     i_danmaku = Danmaku.new(Integer(i_array[0]), Integer(i_array[1]), i_array[2].force_encoding("UTF-8"))
 
-
-    if i_danmaku.uid == man[4] || i_danmaku.uid == man[3]
+    if i_danmaku.uid == man[6] || i_danmaku.uid == man[3] || i_danmaku.uid == man[5]
       danmaku_array = i_danmaku.danmaku_context.split("【", 2)
       if danmaku_array[1]
         danmaku_array[1].delete! '】'
@@ -49,6 +47,11 @@ if file
       end
 
       delta_time = i_danmaku.time - start_time
+
+      # if delta_time < 0
+      #   next
+      # end
+
       delta_time /= 10
       ms = delta_time % 100
       delta_time /= 100
@@ -57,6 +60,10 @@ if file
       min = delta_time % 60
       delta_time /= 60
       h = delta_time
+
+      # if h > 5
+      #   next
+      # end
 
       format = danmaku_array[0]
       if danmaku_array[0].length == 0
@@ -67,9 +74,9 @@ if file
       ass_add_line = "Dialogue: 0,#{h}:#{min}:#{s}.#{ms},#{h}:#{min}:#{s + 1}.#{ms},#{format},,0,0,0,,#{danmaku_array[1]}"
       csv_add_line = "0,#{h}:#{min}:#{s}.#{ms},#{h}:#{min}:#{s + 1}.#{ms},#{format},,0,0,0,,#{danmaku_array[1]}"
       # ass_file.syswrite(ass_add_line)
-      csv_file.syswrite(csv_add_line)
-      # puts ass_add_line
-      puts csv_add_line
+      # csv_file.syswrite(csv_add_line)
+      puts ass_add_line
+      # puts csv_add_line
       # 一技能实战
       # 二技能调试
     end
