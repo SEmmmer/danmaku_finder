4e080a27fa96d56e7a4ea1b9ddbd3dbe6c374553
Shirakami Emmmer <yjhdandan@163.com>
2020-05-14 08:58:12 +0800

    add file

--- drop_miss_black.rb
+++ drop_miss_black.rb
@@ -1,1 +1,20 @@
+# -*- coding: utf-8 -*
 
+require 'mongo'
+
+client = Mongo::Client.new('mongodb://127.0.0.1:27017/test')
+table = client[:uid]
+
+miss_black_file = File.open("drop_list.txt", "rb")
+
+if miss_black_file
+  miss_black_file.each_line do
+  |miss|
+    miss.delete! "\n"
+
+    unless miss
+      next
+    end
+    table.delete_one({:uid => miss})
+  end
+end
