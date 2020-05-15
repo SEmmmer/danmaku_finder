ababf1849f24c4255d8b2586feadca80fb65f542
Shirakami Emmmer <yjhdandan@163.com>
2020-05-15 23:15:08 +0800

    fix insert bug

--- database/db_judge.rb
+++ database/db_judge.rb
@@ -4,10 +4,9 @@ require 'mongo'
 
 client = Mongo::Client.new('mongodb://127.0.0.1:27017/test')
 
-question = client[:question]
-
 begin
   while true
+    question = client[:question]
     uid_col = client[:uid]
     uid_list = []
     uid_col.find.each do
@@ -22,7 +21,7 @@ begin
     end
 
     unless uid_list.include? single["uid"]
-      uid_col.update_one({}, {:uid => single['uid'], :type => "white"})
+      uid_col.insert_one({:uid => single['uid'], :type => "white"})
       $regret_uid = single['uid']
       puts $regret_uid
       puts '请问该言论是否引战，是请输入1，否请直接回车：'
@@ -32,10 +31,10 @@ begin
         uid_col.update_one({:uid => single['uid']}, {:type => "black"})
       end
       question.delete_many(:question => single["danmaku"])
+      puts "over"
     end
   end
 rescue Interrupt
   puts "用户终止进程"
   client[:uid].delete_many({:uid => $regret_uid})
-
 end
