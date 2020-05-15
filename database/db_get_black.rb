2c181a21d5518e4d86b4a942a6cf16af308dbdbc
Shirakami Emmmer <yjhdandan@163.com>
2020-05-15 15:18:58 +0800

    fix drop white

--- database/db_get_black.rb
+++ database/db_get_black.rb
@@ -6,7 +6,7 @@ require 'mongo'
 client = Mongo::Client.new('mongodb://127.0.0.1:27017/test')
 table = client[:uid]
 
-black_file = File.new("new_black.txt", "wb")
+black_file = File.new("database/black.txt", "wb")
 table.find({type: "black"}).each do
 |i|
   black_file.syswrite i['uid'] + "\n"
--- database/drop_white.rb
+++ database/drop_white.rb
@@ -6,7 +6,7 @@ require 'mongo'
 client = Mongo::Client.new('mongodb://127.0.0.1:27017/test')
 table = client[:uid]
 
-table.find({type: "white"}).each do
+table.find({:type => "white"}).each do
 |i|
   table.delete_one i
 end
